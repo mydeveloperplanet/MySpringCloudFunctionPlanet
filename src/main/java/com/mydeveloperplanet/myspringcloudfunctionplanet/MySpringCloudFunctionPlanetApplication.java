2762e62fead33120ba9e446bf402862346c5a0c2
mydeveloperplanet <gunter@mydeveloperplanet.com>
2020-10-17 10:15:58 +0200

    Initial commit

---
+++ src/main/java/com/mydeveloperplanet/myspringcloudfunctionplanet/MySpringCloudFunctionPlanetApplication.java
@@ -0,0 +1,13 @@
+package com.mydeveloperplanet.myspringcloudfunctionplanet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringCloudFunctionPlanetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringCloudFunctionPlanetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mydeveloperplanet/myspringcloudfunctionplanet/MySpringCloudFunctionPlanetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mydeveloperplanet.myspringcloudfunctionplanet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringCloudFunctionPlanetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
