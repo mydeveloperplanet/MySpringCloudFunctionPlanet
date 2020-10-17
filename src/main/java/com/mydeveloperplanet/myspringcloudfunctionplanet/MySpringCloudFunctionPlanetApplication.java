0079432fe8b8a116a41ac7913b2d47f5054604f8
mydeveloperplanet <gunter@mydeveloperplanet.com>
2020-10-17 13:48:27 +0200

    Rewrite the traditional beans approach with the functional style and adapted the tests accordingly

--- src/main/java/com/mydeveloperplanet/myspringcloudfunctionplanet/MySpringCloudFunctionPlanetApplication.java
+++ src/main/java/com/mydeveloperplanet/myspringcloudfunctionplanet/MySpringCloudFunctionPlanetApplication.java
@@ -1,21 +1,30 @@
 package com.mydeveloperplanet.myspringcloudfunctionplanet;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.cloud.function.context.FunctionRegistration;
+import org.springframework.cloud.function.context.FunctionType;
+import org.springframework.cloud.function.context.FunctionalSpringApplication;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.support.GenericApplicationContext;
 
 import java.util.function.Function;
 
-@SpringBootApplication
-public class MySpringCloudFunctionPlanetApplication {
+@SpringBootConfiguration
+public class MySpringCloudFunctionPlanetApplication implements ApplicationContextInitializer<GenericApplicationContext> {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MySpringCloudFunctionPlanetApplication.class, args);
-	}
+    public static void main(String[] args) {
+        FunctionalSpringApplication.run(MySpringCloudFunctionPlanetApplication.class, args);
+    }
 
-	@Bean
-	public Function<String, Boolean> containsCloud() {
-		return value -> value.contains("cloud");
-	}
+    public Function<String, Boolean> containsCloud() {
+        return value -> value.contains("cloud");
+    }
+
+    @Override
+    public void initialize(GenericApplicationContext context) {
+        context.registerBean("containsCloud", FunctionRegistration.class,
+                () -> new FunctionRegistration<>(containsCloud())
+                        .type(FunctionType.from(String.class).to(Boolean.class)));
+    }
 
 }
--- src/test/java/com/mydeveloperplanet/myspringcloudfunctionplanet/MySpringCloudFunctionPlanetApplicationTests.java
+++ src/test/java/com/mydeveloperplanet/myspringcloudfunctionplanet/MySpringCloudFunctionPlanetApplicationTests.java
@@ -2,33 +2,28 @@ package com.mydeveloperplanet.myspringcloudfunctionplanet;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.cloud.function.context.test.FunctionalSpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.asyncDispatch;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-
-@SpringBootTest
-@AutoConfigureMockMvc
+@FunctionalSpringBootTest
+@AutoConfigureWebTestClient
 class MySpringCloudFunctionPlanetApplicationTests {
 
     @Autowired
-    private MockMvc mockMvc;
+    private WebTestClient client;
 
     @Test
-    void doesContainsCloud() throws Exception {
-        MvcResult result = this.mockMvc.perform(post("/containsCloud").contentType(MediaType.TEXT_PLAIN).content("this is a cloud")).andReturn();
-        mockMvc.perform(asyncDispatch(result)).andExpect(content().string("true"));
+    void doesContainsCloud() {
+        client.post().uri("/containsCloud").body(Mono.just("this is a cloud"), String.class).exchange()
+                .expectStatus().isOk().expectBody(String.class).isEqualTo("true");
     }
 
     @Test
-    void doesNotContainsCloud() throws Exception {
-        MvcResult result = this.mockMvc.perform(post("/containsCloud").contentType(MediaType.TEXT_PLAIN).content("this is a function")).andReturn();
-        mockMvc.perform(asyncDispatch(result)).andExpect(content().string("false"));
+    void doesNotContainsCloud() {
+        client.post().uri("/containsCloud").body(Mono.just("this is a function"), String.class).exchange()
+                .expectStatus().isOk().expectBody(String.class).isEqualTo("false");
     }
 
 }
